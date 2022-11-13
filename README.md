501abc9aa1c0f9ab265e63566d8d759a0b131daf
tvlad1234 <vlad.tomoiaga@gmail.com>
2022-11-13 15:44:05 +0200

    add readme, add comments

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # pico-window-example
 Example project using the [pico-window](https://github.com/tvlad1234/pico-window) multitasking environment\
 It runs four example programs: a Fibonacci sequence generator, a program which adds two numbers, a sticky note in which you can simply type text and another one which just prints Hello world in multiple colours.
+
+## Building the project
+Clone this repository with its submodules, then build with *cmake*. The used pins are defined in [main.c](https://github.com/tvlad1234/pico-window-example/blob/main/windowProject/main.c). Program functions are defined in [myApps.c](https://github.com/tvlad1234/pico-window-example/blob/main/windowProject/myApps.c).
--- windowProject/main.c
+++ windowProject/main.c
@@ -1,23 +1,29 @@
 #include "pico/stdlib.h"
+
+// All source files using pico-window must include window.h
 #include "window.h"
 
 #include "myApps.h"
 
+// Define pins
 #define PS2_DATA 1
 #define PS2_CLK 0
 #define VGA_VSYNC 16
 #define VGA_HSYNC 17
-#define VGA_BLUE 18
+#define VGA_RED 18 // Green is 19, Blue is 20
 
 int main()
 {
-  Window_initIO(PS2_DATA, PS2_CLK, VGA_VSYNC, VGA_HSYNC, VGA_BLUE);
+  // Initialize I/O with pins defined above
+  Window_initIO(PS2_DATA, PS2_CLK, VGA_VSYNC, VGA_HSYNC, VGA_RED);
 
+  // Create tasks with windows assigned to them
   Window_createTaskWithWindow(fibonacci, 50, 90, 160, 128, "Fibonacci", BLUE);
   Window_createTaskWithWindow(xPlusY, 240, 90, 160, 128, "x+y", RED);
   Window_createTaskWithWindow(stickyNote, 440, 90, 160, 128, "Sticky note", GREEN);
   Window_createTaskWithWindow(helloWorld, 50, 270, 550, 128, "Hello World!", MAGENTA);
 
+  // Start the FreeRTOS scheduler
   Window_startRTOS();
 
   return 0;
